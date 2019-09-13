e377c1813cb5c82089579c9cec531e9b41d3fb66
?ngel ?lvarez <angel@nan-tic.com>
2019-09-13 11:00:01 +0200

    fix tox and setup

--- setup.py
+++ setup.py
@@ -7,18 +7,19 @@ from setuptools import setup
 import re
 import os
 import io
-try:
-    from configparser import ConfigParser
-except ImportError:
-    from ConfigParser import ConfigParser
+from configparser import ConfigParser
 
-MODULE2PREFIX = {}
+MODULE = 'carrier_zip'
+PREFIX = 'trytonzz'
+MODULE2PREFIX = {
+}
 
 
 def read(fname):
     return io.open(
         os.path.join(os.path.dirname(__file__), fname),
         'r', encoding='utf-8').read()
+
 
 def get_require_version(name):
     if minor_version % 2:
@@ -39,8 +40,6 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'trytonzz_carrier_zip'
-download_url = 'https://bitbucket.org/zikzakmedia/trytond-carrier_zip'
 
 requires = []
 for dep in info.get('depends', []):
@@ -49,28 +48,40 @@ for dep in info.get('depends', []):
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
 requires.append(get_require_version('trytond'))
 
-tests_require = []
-dependency_links = [get_require_version('proteus')]
+tests_require = [get_require_version('proteus')]
+series = '%s.%s' % (major_version, minor_version)
+if minor_version % 2:
+    branch = 'default'
+else:
+    branch = series
+dependency_links = [
+    # ('hg+https://bitbucket.org/zikzakmedia/'
+    #     'trytond-account_payment_gateway@%(branch)s'
+    #     '#egg=trytonzz-account_payment_gateway-%(series)s' % {
+    #         'branch': branch,
+    #         'series': series,
+    #         }),
+    ]
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
-    description='Tryton Carrier Zip Module',
+    description='Tryton Account Payment Gateway Invoice Module',
     long_description=read('README'),
     author='Zikzakmedia SL',
     author_email='zikzak@zikzakmedia.com',
     url='https://bitbucket.org/zikzakmedia/',
-    download_url=download_url,
+    download_url='https://bitbucket.org/zikzakmedia/trytond-%s' % MODULE,
     keywords='',
-    package_dir={'trytond.modules.carrier_zip': '.'},
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.carrier_zip',
-        'trytond.modules.carrier_zip.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.carrier_zip': (info.get('xml', [])
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
             + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
                 'icons/*.svg', 'tests/*.rst']),
         },
@@ -97,9 +108,9 @@ setup(name=name,
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
@@ -107,16 +118,13 @@ setup(name=name,
     license='GPL-3',
     install_requires=requires,
     dependency_links=dependency_links,
+    python_requires='>=3.5',
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    carrier_zip = trytond.modules.carrier_zip
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
-    use_2to3=True,
-    convert_2to3_doctests=[
-        'tests/scenario_carrier_zip.rst',
-        ],
     )
